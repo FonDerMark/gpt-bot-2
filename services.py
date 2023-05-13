9d7974264e1451b665ba18fea92a0930fd129143
Xm0rph <publicnox@gmail.com>
2023-05-14 00:28:58 +0500

    0.0.2

--- services.py
+++ services.py
@@ -2,6 +2,16 @@ import json
 import requests
 
 def _data_to_server(obj):
-    return {
-        'all_data': json.dumps(dict(obj))
-    }
+    if obj.text.split(' ')[0].lower() in ['you', 'forefront', 'poe']:
+        response = {
+            'all_data': json.dumps(dict(obj)),
+            'question': ' '.join(obj.text.split(' ')[1:]),
+            'gpt_mode': obj.text.split(' ')[0].lower(),
+        }
+        print(response['question'])
+        return response
+    else:
+        return {
+            'all_data': json.dumps(dict(obj)),
+            'question': obj.text,
+        }
